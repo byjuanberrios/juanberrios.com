b5de36b6e8fdf9bed8e905d1f2fac11e543fb8f7
Juan Berrios <jberrioscifuentes@gmail.com>
2024-05-14 21:52:59 -0400

    Package astro-icon added + Add icons to floating bar menu links

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import mdx from "@astrojs/mdx";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [mdx()],
+  integrations: [mdx(), icon()],
   redirects: {
     '/blog': '/notes',
     '/posts': '/notes',
