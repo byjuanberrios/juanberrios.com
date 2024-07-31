fe36e05a003ecdda32ca74e9b159c5b8c462be92
Juan Berrios <jberrioscifuentes@gmail.com>
2024-07-31 07:48:53 -0400

    (last.fm) testing runtime variables

--- src/pages/api/new.json.ts
+++ src/pages/api/new.json.ts
@@ -1,7 +1,9 @@
-export const GET = ({}) => {
-  const LASTFM_USER = import.meta.env.LASTFM_USER ?? process.env.LASTFM_USER;
-  const LASTFM_API_KEY =
-    import.meta.env.LASTFM_API_KEY ?? process.env.LASTFM_API_KEY;
+export const GET = (context: any) => {
+  const runtime = context.locals.runtime;
+  const { LASTFM_USER, LASTFM_API_KEY } = runtime.env;
+
+  //   const LASTFM_USER = import.meta.env.LASTFM_USER;
+  //   const LASTFM_API_KEY = import.meta.env.LASTFM_API_KEY;
 
   return new Response(
     JSON.stringify({
