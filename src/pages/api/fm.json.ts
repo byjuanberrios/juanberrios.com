f46d77981f8c724f40b3f0ef4d9de01671a5c806
Juan Berrios <jberrioscifuentes@gmail.com>
2024-07-31 06:42:17 -0400

    (last.fm) testing, testing again...

--- src/pages/api/fm.json.ts
+++ src/pages/api/fm.json.ts
@@ -5,6 +5,9 @@ export async function GET({}) {
 
   const response = await fetch(
     `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${LASTFM_USER}&api_key=${LASTFM_API_KEY}&limit=1&nowplaying=true&format=json`
-  );
-  return new Response(await response.json());
+  )
+    .then((res) => res.json())
+    .then((data) => data);
+
+  return new Response(JSON.stringify(response));
 }
