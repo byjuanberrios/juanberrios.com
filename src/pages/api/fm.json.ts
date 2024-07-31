41918768804d6fb82ebe329ca045a6cb49f5be31
Juan Berrios <jberrioscifuentes@gmail.com>
2024-07-31 07:51:47 -0400

    (last.fm) add context runtime to lastfm test endpoint

--- src/pages/api/fm.json.ts
+++ src/pages/api/fm.json.ts
@@ -1,7 +1,9 @@
-export async function GET({}) {
-  const LASTFM_USER = import.meta.env.LASTFM_USER ?? process.env.LASTFM_USER;
-  const LASTFM_API_KEY =
-    import.meta.env.LASTFM_API_KEY ?? process.env.LASTFM_API_KEY;
+export async function GET(context: any) {
+  //   const LASTFM_USER = import.meta.env.LASTFM_USER ?? process.env.LASTFM_USER;
+  //   const LASTFM_API_KEY =
+  //     import.meta.env.LASTFM_API_KEY ?? process.env.LASTFM_API_KEY;
+  const runtime = context.locals.runtime;
+  const { LASTFM_USER, LASTFM_API_KEY } = runtime.env;
 
   const response = await fetch(
     `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${LASTFM_USER}&api_key=${LASTFM_API_KEY}&limit=1&nowplaying=true&format=json`
