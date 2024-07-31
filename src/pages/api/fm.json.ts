9fc7499fec5b81d1583523c67e5d7c4678533266
Juan Berrios <jberrioscifuentes@gmail.com>
2024-07-31 06:35:32 -0400

    (last.fm) testing again...

---
+++ src/pages/api/fm.json.ts
@@ -0,0 +1,10 @@
+export async function GET({}) {
+  const LASTFM_USER = import.meta.env.LASTFM_USER ?? process.env.LASTFM_USER;
+  const LASTFM_API_KEY =
+    import.meta.env.LASTFM_API_KEY ?? process.env.LASTFM_API_KEY;
+
+  const response = await fetch(
+    `https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=${LASTFM_USER}&api_key=${LASTFM_API_KEY}&limit=1&nowplaying=true&format=json`
+  );
+  return new Response(await response.json());
+}
--- src/pages/api/new.json.ts
+++ src/pages/api/new.json.ts
@@ -1,9 +1,16 @@
-export const GET = ({}) => {
-  return new Response(
-    JSON.stringify({
-      body: JSON.stringify({
-        message: "This was a GET!",
-      }),
-    })
+export async function GET({}) {
+  const response = await fetch(
+    "https://docs.astro.build/assets/full-logo-light.png"
   );
-};
+  return new Response(await response.arrayBuffer());
+}
+
+// export async function GET({ params, request }) {
+//   const response = await fetch(
+//     "https://docs.astro.build/assets/full-logo-light.png"
+//   );
+//   const buffer = Buffer.from(await response.arrayBuffer());
+//   return new Response(buffer, {
+//     headers: { "Content-Type": "image/png" },
+//   });
+// }
