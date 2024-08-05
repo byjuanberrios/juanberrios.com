6a64842feaca5fbd6ab8490223e13536a5fd4e9d
Juan Berrios <jberrioscifuentes@gmail.com>
2024-08-05 17:24:48 -0400

    Change bookmarks categories style

--- src/components/bookmarks/BookmarksCategories.tsx
+++ src/components/bookmarks/BookmarksCategories.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
+
 import { useStore } from "@nanostores/react";
 import { $bookmarkCategory, updateCategory } from "../../store/nano";
+
+import { IconEye, IconEyeClosed } from "@tabler/icons-react";
 
 export const BookmarksCategories = ({
   categories,
@@ -7,24 +11,35 @@ export const BookmarksCategories = ({
   categories: string[];
 }) => {
   const selectedCategory = useStore($bookmarkCategory);
+  const [open, setOpen] = useState(false);
 
   return (
-    <div className="categories">
-      {categories.map((category, i) => (
-        <button
-          key={i}
-          className="category-button"
-          onClick={() => updateCategory(category)}
-          style={{
-            color:
-              selectedCategory === category
-                ? "var(--color-black)"
-                : "var(--color-highlight)",
-          }}
-        >
-          <span>{category}</span>
-        </button>
-      ))}
+    <div className={`categories ${open ? "opened" : "closed"}`}>
+      <header onClick={() => setOpen(!open)}>
+        <h4>
+          <span>Tags</span>
+        </h4>
+        <div className="open-close">
+          {open ? <IconEye /> : <IconEyeClosed />}
+        </div>
+      </header>
+      {open && (
+        <div className="items">
+          {categories.map((category, i) => (
+            <button
+              key={i}
+              className={`category-button ${
+                selectedCategory === category
+                  ? "category-selected"
+                  : "category-nonselected"
+              }`}
+              onClick={() => updateCategory(category)}
+            >
+              <span>{category}</span>
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
